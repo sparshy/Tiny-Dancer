93bff267e47a82a4dc9d97ce506f59b1ccb0168a
Sparsh Yadav <sparshy@gmail.com>
2022-06-01 13:21:49 +0530

    load file bind prop to button

--- src/main.js
+++ src/main.js
@@ -3,24 +3,24 @@ import App from './App.svelte';
 const btn = document.getElementById('btn')
 const filePathElement = document.getElementById('filePath')
 
+
+var app = new App({
+	target: document.getElementById('tiny-mce-holder'),
+	props : {value: 'Load a file'}
+});
+
+
 btn.addEventListener('click', async () => {
     const filePath = await window.electronAPI.openFile()
     filePathElement.innerText = filePath
 	let data = window.electronAPI.fs.readFileSync(filePath, {encoding: 'utf8'})
 
-	new App({
-		target: document.getElementById('tiny-mce-holder'),
-		props : {value: data}
-	});
+	app.$set({value: data})
 
 })
 
 
 
-// var app = new App({
-// 	target: document.getElementById('tiny-mce-holder'),
-// 	props : {value: 'data'}
-// });
 
 // export default app;
 
